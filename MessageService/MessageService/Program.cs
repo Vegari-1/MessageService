7ec2d57be7737ceb8bfb0a8281f2d385a90a8cd4
Nikola <nikolakabasaj6@gmail.com>
2022-08-01 13:29:54 +0200

    feat: monitoring added

--- MessageService/MessageService/Program.cs
+++ MessageService/MessageService/Program.cs
@@ -3,6 +3,14 @@ using MessageService.Repository;
 using MessageService.Repository.Interface;
 using MessageService.Service.Interface;
 using Microsoft.Extensions.Options;
+using OpenTracing;
+using Jaeger.Reporters;
+using Jaeger;
+using Jaeger.Senders.Thrift;
+using Jaeger.Samplers;
+using OpenTracing.Contrib.NetCore.Configuration;
+using OpenTracing.Util;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +36,35 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new() { Title = "MessageService", Version = "v1" });
 });
 
+builder.Services.AddOpenTracing();
+
+builder.Services.AddSingleton<ITracer>(sp =>
+{
+    var serviceName = sp.GetRequiredService<IWebHostEnvironment>().ApplicationName;
+    var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+    var reporter = new RemoteReporter.Builder()
+                    .WithLoggerFactory(loggerFactory)
+                    .WithSender(new UdpSender("host.docker.internal", 6831, 0))
+                    .Build();
+    var tracer = new Tracer.Builder(serviceName)
+        // The constant sampler reports every span.
+        .WithSampler(new ConstSampler(true))
+        // LoggingReporter prints every reported span to the logging framework.
+        .WithLoggerFactory(loggerFactory)
+        .WithReporter(reporter)
+        .Build();
+
+    GlobalTracer.Register(tracer);
+
+    return tracer;
+});
+
+builder.Services.Configure<HttpHandlerDiagnosticOptions>(options =>
+        options.OperationNameResolver =
+            request => $"{request.Method.Method}: {request?.RequestUri?.AbsoluteUri}");
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -44,6 +81,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+// Prometheus metrics
+app.UseMetricServer();
 
 app.Run();
 
