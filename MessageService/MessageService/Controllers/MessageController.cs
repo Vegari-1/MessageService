131b87fee42a1a0026e6fa9a0657b3d0001e13af
Nikola <nikolakabasaj6@gmail.com>
2022-08-01 13:29:54 +0200

    feat: monitoring added

--- MessageService/MessageService/Controllers/MessageController.cs
+++ MessageService/MessageService/Controllers/MessageController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using MessageService.Service.Interface;
 using Microsoft.AspNetCore.Mvc;
+using OpenTracing;
+using Prometheus;
 
 namespace MessageService.Controllers;
 
@@ -10,11 +12,15 @@ public class MessageController : ControllerBase
 {
     private readonly IMessageService _messageService;
     private readonly IMapper _mapper;
+    private readonly ITracer _tracer;
 
-    public MessageController(IMessageService messageService, IMapper mapper)
+    Counter counter = Metrics.CreateCounter("message_service_counter", "message service");
+
+    public MessageController(IMessageService messageService, IMapper mapper, ITracer tracer)
     {
         _messageService = messageService;
         _mapper = mapper;
+        _tracer = tracer;
     }
 }
 
